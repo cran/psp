36bd75d4ff69b40088c050c81264c9855d34ff4d
Lenard Dome <lenarddome@gmail.com>
2023-08-16 16:42:05 +0000

    version 1.0.0

--- R/psp_global.R
+++ R/psp_global.R
@@ -1,59 +1,52 @@
 # function tuning the behaviour of the parameter space partitioning
 # see documentation
-psp_control <- function(radius = 0.1, init = NULL, lower, upper,
-                       pop = 400, cl = NULL,
-                       param_names = NULL,
-                       parallel = TRUE,
-                       cluster_names = NULL,
-                       iterations = 1000) {
+psp_control <- function(radius = 0.1, init, lower, upper,
+                        pop = 400, cl = NULL,
+                        param_names = NULL,
+                        parallel = FALSE,
+                        cluster_names = NULL,
+                        export_objects = NULL,
+                        export_libs = NULL,
+                        iterations = 1000) {
 
     ## error functions
     if (length(upper) != length(lower)) {
         stop("Lower and upper boundaries have different lengths!")
     }
-    if (!is.null(init)) {
-        if (is.vector(init) && length(init) != length(lower)) {
-            stop(paste("init must be either NULL or a vector of same",
-                       "length as the same number of parameters!"))
-        } else if (is.matrix(init) && ncol(init) != length(lower)) {
-            stop(paste("init must be either NULL or a matrix with the same
-                       number of columns as number of parameters!"))
-        }
+
+    if (is.vector(init) && length(init) != length(lower)) {
+        stop(paste("init must be a vector of same",
+                   "length as the number of parameters!"))
+    } else if (is.matrix(init) && ncol(init) != length(lower)) {
+        stop(paste("init must be a matrix with the same
+                   number of columns as number of parameters!"))
     }
-    if (!is.null(param_names)) {
-        if (length(param_names) != length(init)) {
-        stop("param_names and init must have equal length")
-        }
-    }
+
     ## set up hypersphere radius
     if (length(radius) == 1) {
         radius <- rep(radius, length(init))
     }
-    ## set up parallel
-    if (parallel == TRUE && is.null(cl)) {
-        no_cores <- parallel::detectCores()
-        cl <- parallel::makeCluster(no_cores)
-    } else if (parallel == TRUE && !is.null(cl)) {
-        cl <- parallel::makeCluster(cl)
-    }
+
     ## name parameters
     if (is.null(param_names)) {
         param_names <- paste("parameter_", seq(length(init)), sep = "")
     }
-    # export functions to parallel clusters
-    if (!is.null(cluster_names)) {
-        out <- sapply(cluster_names, exists, simplify = TRUE,)
-        if (!all(out)) {
-            stop(paste("You need to load ",
-                       paste(names(which(out == FALSE)), collapse = ", "),
-                       " to your global environment.", sep = ""))
-        }
-    }
+
     ## check if stopping rule is defined appropriately
     if (all(c(pop, iterations) == Inf)) {
         stop("You must set a stopping rule for the function by using either
              iterations or pop in PSPcontrol.")
     }
+
+    ## check exports
+    if (!is.null(cluster_names)) {
+      print(paste("cluster_names is deprecated, please use",
+                  "export_objects instead.\n",
+                  "export_objects is overwritten by cluster_names."))
+      export_objects <- cluster_names
+    }
+
+    ## construct output
     out <- list(radius = radius,
                 pop = pop,
                 cl = cl,
@@ -62,32 +55,62 @@ psp_control <- function(radius = 0.1, init = NULL, lower, upper,
                 upper = upper,
                 parallel = parallel,
                 param_names = param_names,
-                cluster_names = cluster_names,
+                export_objects = export_objects,
+                export_libs = export_libs,
                 iterations = iterations)
+
     return(out)
 }
 
-## Weisstein, Eric W. "Hypersphere Point Picking." From
-## MathWorld--A Wolfram Web Resource.
+## A handy function to set up parallel environment without increasing
+## cyclomatic complexity of the main psp_global
+.parallelize <- function(parallel = FALSE, cl = NULL,
+                        object_names = NULL, lib_names = NULL) {
+    if (parallel == TRUE && is.null(cl)) {
+        no_cores <- parallel::detectCores()
+        cl <- parallel::makeCluster(no_cores)
+    } else if (parallel == TRUE && !is.null(cl)) {
+        cl <- parallel::makeCluster(cl)
+    }
+
+    # HACK: code is clumsy
+    if (parallel == TRUE) {
+        object_names <- c(object_names, "lib_names")
+        parallel::clusterExport(cl, object_names, envir = environment())
+        parallel::clusterEvalQ(cl, {
+          sapply(lib_names,
+                 FUN = function(name) library(name, character.only = TRUE))
+        })
+    }
+    return(cl)
+}
+
+## Weisstein, Eric W. "Hypersphere Point Picking." From MathWorld.
 ## https://mathworld.wolfram.com/HyperspherePointPicking.html
 
-#' generate random distribution from the unit hypersphere relative to 0
-#' scale it by the user-defined radius, then add it to the jumping distribution
-#'
-#' @param init Matrix of coordinates serving as a jumping distribution
-#' @param radius The radius of the hypersphere defining the sampling region
-#' @return Matrix of randomly sampled points within the unit sphere
-psp_hyper <- function(init, radius) {
-    gauss <- rnorm(length(init), mean = 0, sd = 1)
+# generate random distribution from the unit hypersphere relative to jumping distribution
+.psp_hyper <- function(jump, radius) {
+    ## perform simple checks for object types
+    if (is.list(jump)) jump <- unlist(jump)
+    if (!("numeric" %in% is(jump))) jump <- as.numeric(jump)
+    ## generate a random distribution
+    gauss <- rnorm(length(jump), mean = 0, sd = 1)
+    ## sample points from unit hypersphere and scale it by radius
     points <- (1 / sum(sqrt(gauss ^ 2)) * gauss) *
         runif(1, min = 0, max = radius)
-    points_interior <- points + init
+    ## add distance to jumping distribution
+    points_interior <- points + jump
     return(points_interior)
 }
 
-## Parameter Space Partitioning  ------------------------------------------
+## Parameter Space Partitioning
 
-psp_global <- function(fn, control = psp_control()) {
+psp_global <- function(fn, control = psp_control(), ..., quiet = FALSE) {
+
+    .Deprecated(
+        new = "pspGlobal", package = "psp",
+        msg = paste("This function is no longer maintained and is scheduled for removal.\nPlease use pspGlobal instead.")
+    )
 
     ## declare all variables
     ctrl <- do.call(psp_control, as.list(control))
@@ -96,15 +119,17 @@ psp_global <- function(fn, control = psp_control()) {
     init <- ctrl$init
     upper <- ctrl$upper
     lower <- ctrl$lower
-    cl <- ctrl$cl
     pnames <- ctrl$param_names
-    cnames <- ctrl$cluster_names
-    parallel <- ctrl$parallel
-    parallel::clusterExport(cl, cnames,
-                            envir = environment())
+    ex_objects <- ctrl$export_objects
+    ex_libs <- ctrl$export_libs
+
+    ## set up parallel
+    cl <- .parallelize(parallel = ctrl$parallel, cl = ctrl$cl,
+                      object_names = ex_objects, lib_names = ex_libs)
 
     ## define ordinal function
-    FUN <- match.fun(fn)
+    fun <- match.fun(fn)
+    dota <- data.table::data.table
 
     ## miscellaneous variables
     parameter_filled <- FALSE
@@ -114,20 +139,28 @@ psp_global <- function(fn, control = psp_control()) {
 
     ## set first parameter set and ordinal pattern
     parmat_current <- matrix(init, nrow = 1)
-    evaluate <- FUN(parmat_current[1, seq(length(init))])
-    parmat_big <- type.convert(data.frame(cbind(parmat_current, evaluate,
-                                                while_count,
-                                                deparse.level = 0)),
-                               as.is = TRUE)
+    evaluate <- fun(parmat_current[1, seq(length(init))], ...)
+    if (!is.null(dim(evaluate))) evaluate <- list(evaluate)
+
+    ## create big parameter matrix
+    parmat_big <- dota(cbind(parmat_current, evaluate, while_count))
+    colnames(parmat_big) <- c(pnames, "pattern", "iteration")
+
+    ## specify location of ordinal pattern in parmat_big
+    loc <- colnames(parmat_big)[(length(init) + 1)]
+
+    ## does the ordinal pattern have any dimensions?
+    dimension <- any(length(unlist(evaluate)) > 1)
 
     while (!parameter_filled) {
         while_count <- while_count + 1
         ## construct new set of parameters to search
-        new_points <- t(apply(parmat_current, 1,
-                              function(x) {
-                                  psp_hyper(init = x[seq_len(length(init))],
-                                           radius = radius)
-                              }))
+        new_points <-
+            t(apply(parmat_current, 1,
+                    function(x) {
+                        .psp_hyper(jump = x, radius = radius)
+                    }))
+
         ## constrain parameters within bounds
         for (i in seq(length(init))) {
             cbounds <- new_points[, i]
@@ -135,39 +168,68 @@ psp_global <- function(fn, control = psp_control()) {
             cbounds[cbounds < lower[i]] <- lower[i]
             new_points[, i] <- cbounds
         }
+
         # evaluate new_points and record ordinal patterns
-        ifelse(parallel,
-           evaluate <- parallel::parApply(cl, new_points, 1, FUN), # parallel
-           evaluate <- apply(new_points, 1, FUN))                  # no parallel
-        ordinal <- type.convert(cbind(new_points, evaluate, while_count,
-                                      deparse.level = 0),
-                                as.is = TRUE)
+        ifelse(ctrl$parallel,
+           evaluate <- parallel::parApply(cl, new_points, 1, fun, ...),
+           evaluate <- apply(new_points, 1, fun, ...))
+
+        ## save new results with points
+        ordinal <- dota(cbind(new_points, evaluate, while_count))
+        parmat_big <- rbind(parmat_big, ordinal, use.names = FALSE)
+
         ## compare stored and currently produced ordinal responses
-        ## index new regions and update population
-        parmat_big <- rbind(parmat_big, data.frame(ordinal))
-        new_dist <- table(parmat_big[, length(init) + 1])
+        if (dimension) {
+            ## in case ordinal pattern is not a string but has dims
+            pats <- unlist(parmat_big[, loc, with = FALSE], recursive = FALSE)
+            mapping <- match(pats, unique(pats))
+            new_dist <- table(mapping)
+        } else {
+            ## if ordinal pattern is a single string
+            new_dist <- table(unlist(parmat_big[, loc, with = FALSE]))
+            mapping <- unlist(parmat_big[, loc, with = FALSE])
+        }
+
+        ## index regions
         new_index <- which(new_dist < pop)
+        ## if all regions below population limit, keep sampling
+        ## otherwise stop process
         if (length(new_index) > 0) {
             parmat_current <- NULL
             for (i in names(new_dist)) {
-                tmp <- parmat_big[parmat_big[, length(init) + 1] == i,
-                                  seq(length(init))]
-                parmat_current <- rbind(parmat_current,
-                                        matrix(as.numeric(tail(tmp, 1)),
-                                              nrow = 1))
+                ## which(mapping == i) breaks prev sims
+                tmp <- tail(parmat_big[which(mapping == i)], 1)
+                parmat_current <-
+                    rbind(parmat_current,
+                          tmp[, seq_len(length(init)), with = FALSE])
             }
         } else {
             parameter_filled <- TRUE
         }
-        # print is only for debugging as of 2021-01-27T13:08:40+0000
+        ## print information about the current iteration
+        if (!quiet) {
         print(paste("iteration [", while_count, "]: found ",
-                    length(table(parmat_big[, length(init) + 1])),
-                    sep = ""))
+                    length(unique(mapping)), sep = ""))
+        }
+        ## is iteration limit reached, stop sampling
         if (while_count == ctrl$iterations) parameter_filled <- TRUE
     }
-    if (parallel == TRUE) parallel::stopCluster(cl)
-    ordinal_size <- table(parmat_big[, length(init) + 1])
-    colnames(parmat_big) <- c(pnames, "pattern", "iteration")
-    return(list("ps_partitions" = parmat_big,
-                "ps_patterns" = ordinal_size))
+    ## close parallel workplaces
+    if (ctrl$parallel == TRUE) parallel::stopCluster(cl)
+    ## depending on dims of ordinal pattern compile output
+    if (dimension) {
+        pats <- unlist(parmat_big[, loc, with = FALSE], recursive = FALSE)
+        mapping <- match(pats, unique(pats))
+        ordinal_size <- table(mapping, deparse.level = 0)
+        output <- list("ps_partitions" = parmat_big,
+                       "ps_patterns" = ordinal_size,
+                       "ps_ordinal" = unique(pats))
+    } else {
+        ordinal_size <-
+            table(parmat_big[, loc, with = FALSE], deparse.level = 0)
+        output <- list("ps_partitions" = parmat_big,
+                       "ps_patterns" = ordinal_size,
+                       "ps_ordinal" = names(ordinal_size))
+    }
+    return(output)
 }
---
+++ src/pspGlobal.cpp
@@ -0,0 +1,322 @@
+// Copyright 2022 <Lenard Dome> [legal/copyright]
+// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppArmadillo.h>
+#include <fstream>
+
+// [[Rcpp::depends(RcppArmadillo)]]
+using namespace Rcpp;
+using namespace arma;
+
+// utility functions
+// Weisstein, Eric W. "Hypersphere Point Picking." From MathWorld.
+// https://mathworld.wolfram.com/HyperspherePointPicking.html
+// pick new jumping distributions from the unit hypersphere scaled by the radius
+mat HyperPoints(int counts, int dimensions, double radius) {
+  // create a uniform distribution
+  mat hypersphere = randn(counts, dimensions, distr_param(0, 1) );
+  colvec denominator = sum(square(hypersphere), 1);
+  denominator = 1 / sqrt(denominator);
+  // pick points from within unite hypersphere
+  hypersphere = hypersphere.each_col() % denominator;
+  // scale up values by r
+  rowvec rad = randu<rowvec>(dimensions, distr_param(0.0, radius));
+  hypersphere = hypersphere.each_row() % rad;
+  return(hypersphere);
+}
+
+// constrain new jumping distributions within given parameter bounds
+mat ClampParameters(mat jumping_distribution, colvec lower, colvec upper) {
+  for (int i = 0; i < upper.n_elem; i++) {
+    jumping_distribution.col(i).clamp(lower[i], upper[i]);
+  }
+  return(jumping_distribution);
+}
+
+// returns the unique slices of a cube (a 3D array) 
+uvec FindUniqueSlices(cube predictions) {
+  vec predictions_filter(predictions.n_slices, fill::zeros);
+  // filter the same predictions
+  for (uword x = 0; x < predictions.n_slices; x++) {
+    vec current = vectorise( predictions.slice(x) );
+    for (uword y = x + 1; y < predictions.n_slices; y++) {
+      vec base = vectorise( predictions.slice(y) );
+      uvec result = (base == current);
+      if (all(result == 1)) predictions_filter(x) += 1;
+    }
+  }
+  // remove multiple predictions for the comparisons
+  uvec inclusion = find( predictions_filter == 0 );
+  return(inclusion);
+}
+
+// compare two cubes of inequality matrices
+// returns the complete list of unique ordinal matrices
+cube OrdinalCompare(cube discovered, cube predicted) {
+
+  cube drawer(discovered);
+  mat index(predicted.n_slices, discovered.n_slices);
+
+  // carry out the comparisons
+  for (int x = 0; x < predicted.n_slices; x++) {
+    mat current = predicted.slice(x);
+    for (int y = 0; y < discovered.n_slices; y++) {
+      mat base = discovered.slice(y);
+      umat result = (base == current);
+      index(x, y) =  any(vectorise(result) == 0);
+    }
+    if (all(index.row(x) == 1)) {
+      cube update = join_slices(drawer, current);
+      drawer = update;
+    }
+  }
+  return(drawer);
+}
+
+// returns the last evaluated parameters in all chains in the MCMC
+mat LastEvaluatedParameters(cube discovered, cube predicted, mat jumping, mat centers) {
+  mat parameters(centers);
+  mat index(discovered.n_slices, predicted.n_slices);
+  // create index matrix
+  for (uword x = 0; x < discovered.n_slices; x++) {
+    mat current = discovered.slice(x);
+    for (uword y = 0; y < predicted.n_slices; y++) {
+      mat base = predicted.slice(y);
+      umat result = (base == current);
+      index(x, y) =  any(vectorise(result) == 0);
+    }
+    if (all(index.row(x))) {
+      //  if there is a new region, appeng params to centers
+      parameters.insert_rows(parameters.n_rows, jumping.rows( find(index.row(x) == 1, 1, "last") ));
+    } else {
+      // if there is an old region in predicted, update center
+      parameters.row(x) = jumping.rows( find(index.row(x) == 0, 1, "last") );
+    }
+    // replace old centers with new ones
+  }
+  return(parameters);
+}
+
+// count ordinal patterns
+rowvec CountOrdinal(cube updated_ordinal, cube predicted, rowvec counts) {
+  mat index(updated_ordinal.n_slices, predicted.n_slices);
+  rowvec new_counts = counts;
+  new_counts.resize(updated_ordinal.n_slices);
+  for (uword x = 0; x < updated_ordinal.n_slices; x++) {
+    mat current = updated_ordinal.slice(x);
+    for (uword y = 0; y < predicted.n_slices; y++) {
+      mat base = predicted.slice(y);
+      umat result = (base == current);
+      if (all(vectorise(result) == 1)) {
+        new_counts[x] += 1;
+      }
+    }
+  }
+  return(new_counts);
+}
+
+// match jumping distributions to ordinal ordinal_patterns
+// returns a column uvec of slice IDs corresponding to each set in jumping_distribution
+vec MatchJumpDists(cube updated_ordinal, cube predicted) {
+  mat index(updated_ordinal.n_slices, predicted.n_slices);
+  vec matches(predicted.n_slices);
+  for (uword x = 0; x < updated_ordinal.n_slices; x++) {
+    mat current = updated_ordinal.slice(x);
+    for (uword y = 0; y < predicted.n_slices; y++) {
+      mat base = predicted.slice(y);
+      umat result = (base == current);
+      if (all(vectorise(result) == 1)) {
+        matches(y) = x;
+      }
+    }
+  }
+  return(matches + 1); // add one as c++ starts from 0
+}
+
+// create local csv file for storing coordinates
+void CreateFile(CharacterVector names, std::string path_to_file) {
+  std::ofstream outFile(path_to_file.c_str());
+  outFile << "iteration,";
+  for (uword i = 0; i < names.size(); i++) {
+    outFile << names[i];
+    outFile << + ",";
+  }
+  outFile << "pattern,\n";
+}
+
+// writes rows to csv file
+void WriteFile(int iteration, mat evaluation, vec matches,
+  std::string path_to_file) {
+  // open file stream connection
+  std::ofstream outFile(path_to_file.c_str(), std::ios::app);
+  int rows = evaluation.n_rows;
+  int columns = evaluation.n_cols;
+  for (uword i = 0; i < rows; i++) {
+    outFile << iteration;
+    outFile << ",";
+    for (uword k = 0; k < columns; k++) {
+      outFile << evaluation(i, k);
+      outFile << ",";
+    }
+    outFile << matches(i);
+    outFile << ",\n";
+  }
+}
+
+// [[Rcpp::export]]
+List pspGlobal(Function model, Function discretize, List control, bool save = false,
+               std::string path = ".", std::string extension = ".csv", bool quiet = false) {
+  // setup environment
+  bool parameter_filled = false;
+  int iteration = 0;
+  uvec underpopulated = { 0 };
+
+  // import thresholds from control
+  int max_iteration = as<int>(control["iterations"]);
+  int population = as<int>(control["population"]);
+
+  if (!max_iteration) {
+    max_iteration = datum::inf;
+  }
+
+  if (!population) {
+    population =  datum::inf;
+  }
+
+  if (population == datum::inf && max_iteration == datum::inf) {
+    stop("A resonable threshold must be set by either adjusting iteration or population.");
+  }
+
+  double radius  = as<double>(control["radius"]);
+  mat init = as<mat>(control["init"]);
+
+  colvec lower = as<colvec>(control["lower"]);
+  colvec upper = as<colvec>(control["upper"]);
+  int dimensions = init.n_cols;
+  // do some basic error checks
+  if (dimensions != lower.n_elem || dimensions != upper.n_elem) {
+    stop("init, lower and upper must have the same length.");
+  }
+  int dimensionality = as<int>(control["dimensionality"]);
+  int response_length = as<int>(control["responses"]);
+  rowvec counts(1, fill::ones);  // keeps track of the population of ordinal regions
+  cube filtered; // stores all unique predictions
+  cube storage;  //  stores all unique ordinal patterns
+  CharacterVector parameter_names = as<CharacterVector>(control["parameter_names"]);
+  if (parameter_names.size() != dimensions) {
+    stop("Length of param_names must equal to the number of dimensions");
+  }
+  CharacterVector stimuli_names = as<CharacterVector>(control["stimuli_names"]);
+  List out;
+
+  // seed has to be set at the global R level
+  // see Documentation about the sampling
+  Rcpp::Environment base_env("package:base");
+  Rcpp::Function set_seed_r = base_env["set.seed"];
+
+  // evaluate first parameter sets
+  mat last_eval = init;
+  mat jumping_distribution = init;
+  mat continuous(jumping_distribution.n_rows, response_length);
+
+
+  // create first ordinal storage
+  cube ordinal(dimensionality, dimensionality, jumping_distribution.n_rows);
+
+
+  // evaluate jumping distributions
+  for (uword i = 0; i < jumping_distribution.n_rows; i++) {
+    NumericVector probabilities = model(jumping_distribution.row(i));
+    NumericMatrix teatime = discretize(probabilities);
+    const rowvec& responses = as<rowvec>(probabilities);
+    continuous.row(i) = responses;
+    const mat& evaluate = as<mat>(teatime);
+    ordinal.slice(i) = evaluate;
+  }
+
+  // compare ordinal patterns to stored ones and update list
+  uvec include = FindUniqueSlices(ordinal);
+
+  // update last evaluated parameters
+  last_eval = LastEvaluatedParameters(storage, ordinal.slices(include),
+                                      jumping_distribution.rows(include),
+                                      last_eval);
+
+  storage = OrdinalCompare(storage, ordinal.slices(include));
+  counts = CountOrdinal(storage, ordinal, counts);
+
+  ////////////////////////////////////////////////////////////////
+
+  if (save) {
+    vec match = MatchJumpDists(storage, ordinal);
+    CreateFile(parameter_names, path + "_parameters" + extension);
+    CreateFile(stimuli_names, path + "_continuous" + extension);
+    WriteFile(0, jumping_distribution, match, path + "_parameters" + extension);
+    WriteFile(0, continuous, match, path + "_continuous" + extension);
+  }
+
+  // run parameter space partitioning until parameter is filled
+  while (!parameter_filled) {
+    // update iteration
+    iteration += 1;
+
+    if (!quiet) {
+      Rprintf("Iteration: [%i]\n", iteration);
+    }
+
+    // reset the seed
+    int pool =  as<int>(Rcpp::sample(10000000, 1));
+    set_seed_r(pool); 
+
+    // generate new jumping distributions from ordinal patterns with counts < population
+    mat jumping_distribution = HyperPoints(underpopulated.n_elem,
+                                           dimensions, radius);
+    jumping_distribution = jumping_distribution + last_eval.rows(underpopulated);
+    jumping_distribution = ClampParameters(jumping_distribution, lower, upper);
+    // allocate cube for ordinal predictions
+    ordinal.resize(dimensionality, dimensionality, jumping_distribution.n_rows);
+    continuous.resize(jumping_distribution.n_rows, response_length);
+
+    // evaluate jumping distributions
+    for (uword i = 0; i < jumping_distribution.n_rows; i++) {
+      NumericVector probabilities = model(jumping_distribution.row(i));
+      NumericMatrix teatime = discretize(probabilities);
+      const mat& evaluate = as<mat>(teatime);
+      ordinal.slice(i) = evaluate;
+    }
+
+    // compare ordinal patterns to stored ones and update list
+    uvec include = FindUniqueSlices(ordinal);
+
+    // update last evaluated parameters
+    last_eval = LastEvaluatedParameters(storage, ordinal.slices(include),
+                                        jumping_distribution.rows(include),
+                                        last_eval);
+
+    storage = OrdinalCompare(storage, ordinal.slices(include));
+
+    // update counts of ordinal patterns
+    counts = CountOrdinal(storage, ordinal, counts);
+    underpopulated = find( counts < population );
+
+    // write data to disk
+    if (save) {
+      // index locations of currently found patterns in storage
+      vec match = MatchJumpDists(storage, ordinal);
+      WriteFile(iteration, jumping_distribution, match, path + "_parameters" + extension);
+      WriteFile(0, continuous, match, path + "_continuous" + extension);
+    }
+
+    // check if either of the parameter_filled thresholds is reached
+    if (iteration == max_iteration || underpopulated.n_elem == 0) {
+      parameter_filled = TRUE;
+    }
+  }
+
+  // compile output including ordinal patterns and their frequencies
+  out = Rcpp::List::create(
+    Rcpp::Named("ordinal_patterns") = storage,
+    Rcpp::Named("ordinal_counts") = counts,
+    Rcpp::Named("iterations") = iteration);
+
+  return(out);
+}
